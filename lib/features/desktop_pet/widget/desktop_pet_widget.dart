7e9730a1111c140d1ceab887fff4dae594cabc91
MeLaStIk <melastik@jusquad.ru>
2026-07-27 12:19:15 +0300

    Add character layout constraints with SizedBox
    - Added SizedBox to define the character workspace
    - Moved size responsibility to DesktopPetWidget
    - Kept CharacterWidget focused on character composition
    - Improved widget hierarchy following Single Responsibility Principle
    - Reduced coupling between layout and character implementation
    - Prepared architecture for future sprite rendering

--- lib/features/desktop_pet/widget/desktop_pet_widget.dart
+++ lib/features/desktop_pet/widget/desktop_pet_widget.dart
@@ -7,6 +7,10 @@ class DesktopPetWidget extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    return CharacterWidget();
+    return SizedBox(
+      width: 256,
+      height:256,
+      child:CharacterWidget(),
+    );
   }
 }
